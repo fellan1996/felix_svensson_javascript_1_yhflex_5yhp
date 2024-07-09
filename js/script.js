c53192b46e3a99d509ccede14699f328d8bc7d04
Felix Svensson <felixsvensson1996@gmail.com>
2024-07-09 14:28:22 +0200

    fixed small bug

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ form.addEventListener("submit", fetchData);
 const wrapper = document.getElementById("wrapper");
 let grid;
 
-const newSearchBtn = document.getElementById("new-search");
-newSearchBtn.addEventListener("click", () => location.reload());
-
 async function fetchData(event) {
   event.preventDefault();
   wrapper.innerHTML = null;
@@ -20,23 +17,26 @@ async function fetchData(event) {
 
     const data = await response.json();
 
-    if(data.message === "Not Found") {wrapper.innerHTML = "no countries matched the search"
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    };
+    if (data.message === "Not Found") {
+      wrapper.innerHTML = "no countries matched the search";
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
     const sortedData = data.sort((a, b) => b.population - a.population);
 
     grid ? updateData(sortedData) : displayData(sortedData);
-    displayData(sortedData);
+    // displayData(sortedData);
   } catch (error) {
     if (error instanceof TypeError) {
-        if(error.message.includes("NetworkError")) {
-            wrapper.innerHTML = "Something went wrong, check if you have connection and reload the page"
-        }
-        console.error("TypeError", error);
-    }else if (error instanceof ReferenceError) {
-        wrapper.innerHTML = "Something went wrong, check if you have connection and reload the page"
-        console.error("ReferenceError", error);
+      if (error.message.includes("NetworkError")) {
+        wrapper.innerHTML =
+          "Something went wrong, check if you have connection and reload the page";
+      }
+      console.error("TypeError", error);
+    } else if (error instanceof ReferenceError) {
+      wrapper.innerHTML =
+        "Something went wrong, check if you have connection and reload the page";
+      console.error("ReferenceError", error);
     } else {
       console.error("Error fetching data:", error);
     }
